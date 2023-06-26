84663cb1f5e746a4a456eb9740746b95b7ac9e0f
Mateja Cimpersak <matejacimpersak@gmail.com>
2023-06-26 23:05:51 +0200

    build funcion for single round of game against computer's random choice

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-function playRound(playerSelection, computerSelection) {
-  
+function getComputerChoice() {
+  let computerChoices = ["rock", "paper", "scissors"];
+  return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
- 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    return "You win!";
+  } else if (
+    (computerSelection == "scissors" && playerSelection == "paper") ||
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "paper" && playerSelection == "rock")
+  ) {
+    return "You lose!";
+  } else if (playerSelection == computerSelection) {
+    return "It's a tie";
+  }
+}
+
+let playerSelectionPrompt = prompt("Rock, Paper, or Scissors");
+let playerSelection = playerSelectionPrompt.toLowerCase();
+let computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
